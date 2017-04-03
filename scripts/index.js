9f57ccce66253b181d3735c54fe49e73555cac80
Kyle Anderson <kmanderson12@gmail.com>
2017-04-03 15:36:00 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -18,12 +18,10 @@ $(document).ready(function() {
   
   $(window).scroll(function(){
     if ($(this).scrollTop() > 300) {
-      if($("#circle").hasClass("hidden") {
-      $("#circle").removeClass("hidden",1000);
-    }
+      $("#circle").fadeIn(1000);
     }
     else {
-      $("#circle").addClass("hidden",500);
+      $("#circle").fadeOut(500);
     }
   })
 
