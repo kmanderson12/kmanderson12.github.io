9d8041cb377c5f2efa057b6c94d6bd940e5f495b
Kyle Anderson <kmanderson12@gmail.com>
2017-04-03 13:56:44 -0500

    Added smooth scroll.

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,17 @@
-$(document).ready(function(){
-  
-  
+$(document).ready(function() {
+
+  //smooth scroll to target learned from Chris Coyier http://codepen.io/chriscoyier/pen/dpBMVP
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+
 });
